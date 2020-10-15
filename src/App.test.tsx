bfc2d50ab2469c546d5c7a26ce3b3243d2680af6
Nicole Budiman <nicole.budiman@thoughtworks.com>
2020-10-16 04:40:50 +0800

    simple test app

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,13 +2,23 @@ import React from "react";
 import { render } from "@testing-library/react";
 import App from "./App";
 
-test("renders learn react link", () => {
+test("renders heading", () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/hello react/i);
+  const linkElement = getByText(/learn typescript/i);
   expect(linkElement).toBeInTheDocument();
 });
 
 it("should render the paragraph", () => {
   const { getByText } = render(<App />);
   expect(getByText("With TDD")).toBeInTheDocument();
+});
+
+it("should generate a label", () => {
+  const { getByText } = render(<App />);
+  expect(getByText(/react/i)).toBeInTheDocument();
+});
+
+it("should generate a greeting", () => {
+  const { getByText } = render(<App />);
+  expect(getByText(/hello jello/i)).toBeInTheDocument();
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
 import React from "react";
 
 function App() {
+  function label(): string {
+    return "React";
+  }
+
+  function greet(name: string): string {
+    return `Hello ${name}`;
+  }
+
   return (
     <div>
-      <h1>Hello React + Typescript</h1>
+      <h1>Learn Typescript</h1>
       <p>With TDD</p>
+      <h1>{label()}</h1>
+      <h2>{greet("jello")}</h2>
     </div>
   );
 }
