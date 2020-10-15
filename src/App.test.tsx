04ac44b7dba69a5f7bd6ca0de2460a38d793b386
Nicole Budiman <nicole.budiman@thoughtworks.com>
2020-10-15 16:28:20 +0800

    chore: add react testing library

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,14 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders learn react link", () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const linkElement = getByText(/hello react/i);
   expect(linkElement).toBeInTheDocument();
+});
+
+it("should render the paragraph", () => {
+  const { getByText } = render(<App />);
+  expect(getByText("With TDD")).toBeInTheDocument();
 });
