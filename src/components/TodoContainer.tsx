df744d0a7b4b949094bf638d182025953ec3f146
Nicole Budiman <nicole.budiman@thoughtworks.com>
2020-10-26 15:56:47 +0800

    style: todo container

--- src/components/TodoContainer.tsx
+++ src/components/TodoContainer.tsx
@@ -2,8 +2,9 @@ import React from "react";
 import TodoList from "./TodoList";
 import { actions as todoActions } from "../ducks/todo";
 import { State } from "../common/interface/types";
-import { NewTodo } from "../NewTodo";
+import { NewTodo } from "./NewTodo";
 import { useSelector, useDispatch } from "react-redux";
+import styles from "./TodoContainer.module.css";
 
 export const TodoContainer = () => {
   const todos = useSelector<State, State["todos"]>((state) => state.todos);
@@ -23,14 +24,17 @@ export const TodoContainer = () => {
 
   return (
     <React.Fragment>
-      <h1>Todo List</h1>
-      <NewTodo addTodoItem={addTodoItem} />
-      <TodoList
-        todos={todos}
-        toggleStatus={toggleStatus}
-        removeTodoItem={removeTodoItem}
-      />
+      <h1 className={styles.header}>Todo List</h1>
+      <div className={styles.todoContainer}>
+        <NewTodo addTodoItem={addTodoItem} />
+        <TodoList
+          todos={todos}
+          toggleStatus={toggleStatus}
+          removeTodoItem={removeTodoItem}
+        />
+      </div>
     </React.Fragment>
+
   );
 };
 
