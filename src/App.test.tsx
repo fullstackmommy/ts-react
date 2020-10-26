92a0656fd7a7f3a232f641dd742a58d51f19fec8
Nicole Budiman <nicole.budiman@thoughtworks.com>
2020-10-26 15:57:15 +0800

    style: app

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -4,21 +4,6 @@ import App from "./App";
 
 test("renders heading", () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn typescript/i);
+  const linkElement = getByText(/react - redux - typescript/i);
   expect(linkElement).toBeInTheDocument();
-});
-
-it("should render the paragraph", () => {
-  const { getByText } = render(<App />);
-  expect(getByText("With TDD")).toBeInTheDocument();
-});
-
-it("should generate a label", () => {
-  const { getByText } = render(<App />);
-  expect(getByText(/react/i)).toBeInTheDocument();
-});
-
-it("should generate a greeting", () => {
-  const { getByText } = render(<App />);
-  expect(getByText(/hello jello/i)).toBeInTheDocument();
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,9 @@
 import React from "react";
 
 function App() {
-  function label(): string {
-    return "React";
-  }
-
-  function greet(name: string): string {
-    return `Hello ${name}`;
-  }
-
   return (
     <div>
-      <h1>Learn Typescript</h1>
-      <p>With TDD</p>
-      <h1>{label()}</h1>
-      <h2>{greet("jello")}</h2>
+      <h1>React - Redux - Typescript</h1>
     </div>
   );
 }
